a76c4d4ea36413f5a71cd1db78e1685be8242453
JuanCarlosLopezHortelano <juancarloslhhellin@gmail.com>
2025-02-21 14:02:36 +0100

    feat: Conectado frontend con backend

--- src/app/client/client.service.ts
+++ src/app/client/client.service.ts
@@ -1,18 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Client } from './model/Client';
-import {CLIENT_DATA} from './model/mock-clients'
+
 @Injectable({
-  providedIn: 'root'
+providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  constructor(
+    private http:HttpClient,
+  ) { }
+
+  private baseUrl = 'http://localhost:8080/client';
 
   getClients(): Observable<Client[]>{
-    return  of(CLIENT_DATA);
+   return this.http.get<Client[]>(this.baseUrl)
   }
 
-  saveClient(client: Client): Observable<Client>{return of(null)}
-  deleteClient(idClient: number): Observable<any>{return of(null)}
+  saveClient(client: Client): Observable<Client>{
+    const {id} = client;
+    const url = id ? `${this.baseUrl}/${id}`:this.baseUrl;
+    return this.http.put<Client>(url,client);
+
+  }
+  deleteClient(idClient: number): Observable<any>{
+    
+    return this.http.delete(`${this.baseUrl}/${idClient}`);
+  }
 }
